86698d8dde7a93c679dc3b09eb2e78efc3bd9094
Ng'ash_254 <samuelnganga.w@gmail.com>
2024-02-06 16:47:29 +0300

    Read data from state

--- src/Data/users.js
+++ src/Data/users.js
@@ -8,17 +8,4 @@ export const users = [
 		name: "Christopher Smith",
 		email: "christopher.smith@example.com",
 	},
-	{ id: 6, name: "Jessica Garcia", email: "jessica.garcia@example.com" },
-	{ id: 7, name: "Alexander Chen", email: "alexander.chen@example.com" },
-	{
-		id: 8,
-		name: "Isabella Thompson",
-		email: "isabella.thompson@example.com",
-	},
-	{
-		id: 9,
-		name: "Andrew Hernandez",
-		email: "andrew.hernandez@example.com",
-	},
-	{ id: 10, name: "Olivia Nguyen", email: "olivia.nguyen@example.com" },
 ];
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,94 @@
-import { Grid, Box, Typography, Button } from "@mui/material";
+import {
+	Grid,
+	Box,
+	Paper,
+	Typography,
+	Button,
+	TableContainer,
+	Table,
+	TableHead,
+	TableBody,
+	TableRow,
+	TableCell,
+} from "@mui/material";
+
+import { useSelector } from "react-redux";
 
 export default function Home() {
+	const users = useSelector((state) => state.users);
+
 	return (
 		<Grid>
-			<Box>
-				<Typography variant="h4">
+			<Box sx={{ pb: 2 }}>
+				<Typography variant="h4" sx={{ pb: 2 }}>
 					User Management with React and Redux
 				</Typography>
-				<Button variant="contained">Create User</Button>
+				<Button variant="contained" color="success">
+					Create User
+				</Button>
 			</Box>
+
+			<TableContainer component={Paper}>
+				<Table>
+					<TableHead>
+						<TableRow>
+							<TableCell align="right">
+								ID
+							</TableCell>
+							<TableCell align="right">
+								NAME
+							</TableCell>
+							<TableCell align="right">
+								EMAIL
+							</TableCell>
+							<TableCell align="right">
+								ACTIONS
+							</TableCell>
+						</TableRow>
+					</TableHead>
+
+					<TableBody>
+						{users.map((user, index) => (
+							<TableRow key={index}>
+								<TableCell align="right">
+									{
+										user.id
+									}
+								</TableCell>
+								<TableCell align="left">
+									{
+										user.name
+									}
+								</TableCell>
+								<TableCell align="right">
+									{
+										user.email
+									}
+								</TableCell>
+								<TableCell align="right">
+									<Button
+										variant="contained"
+										color="secondary"
+										size="small"
+										sx={{
+											mx: 2,
+										}}
+									>
+										Edit
+									</Button>
+									<Button
+										variant="contained"
+										color="error"
+										size="small"
+									>
+										Delete
+									</Button>
+								</TableCell>
+							</TableRow>
+						))}
+					</TableBody>
+				</Table>
+			</TableContainer>
 		</Grid>
 	);
 }
